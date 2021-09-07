f6b3a7a11721d1c77c3941ef32270ae8c816976f
Arup Mondal <mondalconnect17@gmail.com>
2021-09-07 21:34:58 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// {/* <script src="https://kit.fontawesome.com/498bf66a0a.js" crossorigin="anonymous"></script> */}
+
+let screen = document.getElementById('screen');
+let buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log(buttonText);
+
+        if (buttonText == 'X') {
+            buttonText = '*';
+
+            screenValue += buttonText;
+            screen.value = screenValue;
+
+        } else if (buttonText == 'C') {
+            screenValue = '';
+            screen.value = screenValue;
+        } else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
